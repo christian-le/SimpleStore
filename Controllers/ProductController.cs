9ca9377d673c01c9f95f2978b1b02b57d02955b3
christian-le <lehongchau1205@gmail.com>
2019-03-22 16:16:59 +0700

    add simple store

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleStore.Infrastructure.Services;
+using SimpleStore.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductService _productService;
+        public int pageSize = 3;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Index(int productPage = 1)
+        {
+            var products = await _productService.GetAll();
+
+            var model = new ProductListViewModel()
+            {
+                Products = products.OrderBy(p => p.Id)
+                .Skip((productPage - 1) * pageSize)
+                .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemPerPage = pageSize,
+                    TotalItems = products.Count()
+                }
+            };
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleStore.Data;
+using SimpleStore.Infrastructure.Data;
+using SimpleStore.Infrastructure.Interfaces;
+using SimpleStore.Infrastructure.Models;
+using SimpleStore.Infrastructure.Entities;
+using SimpleStore.Infrastructure.Services;
+using SimpleStore.Models;
+
+namespace SimpleStore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            //Add Identity DbContext
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("IdentityConnection")));
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddDefaultUI(UIFramework.Bootstrap4)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            //Add SimpleStore DbContext
+            services.AddDbContext<SinpleStoreDbContext>(c =>
+                c.UseSqlServer(Configuration.GetConnectionString("SimpleStoreConnection")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(EfRepository<>));
+            services.AddScoped<IProductRepository,ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMemoryCache();
+            services.AddSession();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+            app.UseSession();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "pagination",
+                    template: "Product/page{productPage}",
+                    defaults: new { Controller = "Product", action = "Index" });
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Product}/{action=Index}/{id?}");
+            });
+
+            SeedSimpleStoreData.EnsurePopolated(app);
+        }
+    }
+}
